61fa7b23c3671f0c7244330d292791629c9d1350
Lesandu Luthmin <lesanduluthmmen8@gmail.com>
2025-03-16 12:16:57 +0530

    made the technical skills compornent more reusable

--- src/Body/Section/TechnicalSkills/TechnicalSkills.jsx
+++ src/Body/Section/TechnicalSkills/TechnicalSkills.jsx
@@ -1,31 +1,22 @@
 import styles from "./TechnicalSkills.module.css"
 import SkillBox from "../../Compornents/SkillBox";
-import csharp  from "../../../../public/csharp.png"
-import javascript  from "../../../../public/javascript.png"
-import python  from "../../../../public/Python.png"
-import css  from "../../../../public/Css.png"
-import mysql  from "../../../../public/Mysql.png"
-import html  from "../../../../public/Html.png"
-import php  from "../../../../public/php.png"
-import react from "../../../../public/react.png"
 
 
 
-function TechnicalSkills(){
+
+function TechnicalSkills({header,hightlight,skillData}){
+
+    const Data = skillData;
     return(
-        <section>
-            <h2 className={styles.header}>Professional <span className={styles.hightlight}>Skills</span></h2>
+        <section className={styles.section}>
+            <h2 className={styles.header}>{header} <span className={styles.hightlight}>{hightlight}</span></h2>
             <div className={styles.mainContainer}>
-                <SkillBox name="HTML"  image={html}></SkillBox>
-                <SkillBox name="CSS"  image={css}></SkillBox>
-                <SkillBox name="javascript"  image={javascript}></SkillBox>
-                <SkillBox name="React"  image={react}></SkillBox>
+                {Data.map((skill)=>{
+                    return(
+                        <SkillBox image={skill.image} name={skill.name}></SkillBox>
 
-                <SkillBox name="my sql"  image={mysql}></SkillBox>
-                <SkillBox name="python"  image={python}></SkillBox>
-                <SkillBox name="php"  image={php}></SkillBox>
-
-                <SkillBox name="C#"  image={csharp}></SkillBox>
+                    )
+                })}
             </div>
         </section>
     )
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,56 @@
+// Importing the reusable Compornents
 import Footer from "../../Footer/Footer";
 import Header from "../../Header/Header";
 import Myself from "../../Body/Section/Myself/Myself";
 import TechnicalSkills from "../../Body/Section/TechnicalSkills/TechnicalSkills";
 
+// importing the images file path
+import HTML from "../../../public/Html.png"
+import CSS from "../../../public/Css.png"
+import PHP from "../../../public/php.png"
+import Javascript from "../../../public/javascript.png"
+import MySql from "../../../public/Mysql.png"
+import Python from "../../../public/Python.png"
+import React from "../../../public/react.png"
+import Csharp from "../../../public/csharp.png"
+import dart from "../../../public/dart.png"
+import flutter  from "../../../public/flutter.png"
+import andriod from "../../../public/android.png"
+
+
+
+
+
+// Function which returns the page
 function About(){
+
+    const skills =[
+        {name:"HTML",image:HTML},
+        {name:"CSS",image:CSS},
+        {name:"Javascript",image:Javascript},
+        {name:"Php",image:PHP},
+        {name:"My Sql",image:MySql},
+        {name:"Python",image:Python},
+        {name:"React",image:React},
+        {name:"C#",image:Csharp},
+
+
+
+        
+    ]
+
+    const learning =[
+        {name:"Dart",image:dart},
+        {name:"Flutter",image:flutter},
+        {name:"Andriod Studio",image:andriod},
+        {name:"Python Machine Learning",image:Python}
+    ]
     return(
         <>
         <Header></Header>
         <Myself></Myself>
-        <TechnicalSkills></TechnicalSkills>
+        <TechnicalSkills header={"Professional"} hightlight={"Skills"} skillData={skills}></TechnicalSkills>
+        <TechnicalSkills header={"What I'm Currently"} hightlight={"Learning"}skillData={learning} ></TechnicalSkills>
         <Footer></Footer>
         </>
     );
