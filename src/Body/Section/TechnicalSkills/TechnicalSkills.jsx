4c398176cdf2608eedb6b17f0df37cfe239dfe32
Lesandu Luthmin <lesanduluthmmen8@gmail.com>
2025-03-16 10:50:44 +0530

    added react to the proffessional Skills

--- src/Body/Section/TechnicalSkills/TechnicalSkills.jsx
+++ src/Body/Section/TechnicalSkills/TechnicalSkills.jsx
@@ -1,5 +1,13 @@
 import styles from "./TechnicalSkills.module.css"
 import SkillBox from "../../Compornents/SkillBox";
+import csharp  from "../../../../public/csharp.png"
+import javascript  from "../../../../public/javascript.png"
+import python  from "../../../../public/Python.png"
+import css  from "../../../../public/Css.png"
+import mysql  from "../../../../public/Mysql.png"
+import html  from "../../../../public/Html.png"
+import php  from "../../../../public/php.png"
+import react from "../../../../public/react.png"
 
 
 
@@ -8,7 +16,16 @@ function TechnicalSkills(){
         <section>
             <h2 className={styles.header}>Professional <span className={styles.hightlight}>Skills</span></h2>
             <div className={styles.mainContainer}>
-                <SkillBox name="lesandu" ></SkillBox>
+                <SkillBox name="HTML"  image={html}></SkillBox>
+                <SkillBox name="CSS"  image={css}></SkillBox>
+                <SkillBox name="javascript"  image={javascript}></SkillBox>
+                <SkillBox name="React"  image={react}></SkillBox>
+
+                <SkillBox name="my sql"  image={mysql}></SkillBox>
+                <SkillBox name="python"  image={python}></SkillBox>
+                <SkillBox name="php"  image={php}></SkillBox>
+
+                <SkillBox name="C#"  image={csharp}></SkillBox>
             </div>
         </section>
     )
