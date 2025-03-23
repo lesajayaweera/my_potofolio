44e48483e02ae63dc8a737c7011ee6ded2fa43e6
Lesandu Luthmin <lesanduluthmmen8@gmail.com>
2025-03-23 11:15:24 +0530

    made the header more responsive with a header in a shape of a "l"

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,29 +1,53 @@
-import styles from "./Header.module.css"
-import { Link } from "react-router-dom"
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
+function Header() {
+  const [MenuToggle, SetMenuToggle] = useState(false);
 
+  const toggleMenu = () => {
+    SetMenuToggle(!MenuToggle);
+  };
 
-function Header (){
-    return(
-        <>
-            <header className={styles.header}>
-                <h1 className={styles.h1}>Lesandu Jayaweera</h1>
-                <nav className={styles.nav}>
-                    
-                    <ul className={styles.ul}>
-                        
-                        <li className={styles.li}><Link to="/">Home</Link></li>
-                        <li className={styles.li}><Link to="/about">About me</Link></li>
-                        <li className={styles.li}><Link to="/projects">Projects</Link></li>
-                        <li className={styles.li}><Link to="/resume">Resume</Link></li>
-                    </ul>
-                    
-
-                </nav>
-            </header>
-            
-        </>
-    )
+  return (
+    <>
+      <header className={styles.header}>
+        <h1 className={styles.h1}>Lesandu Jayaweera</h1>
+        <nav className={`${styles.nav} ${MenuToggle ? styles.active : ""}`}>
+          <ul className={styles.ul}>
+            <li className={styles.li}>
+              <Link to="/" onClick={toggleMenu}>
+                Home
+              </Link>
+            </li>
+            <li className={styles.li}>
+              <Link to="/about" onClick={toggleMenu}>
+                About me
+              </Link>
+            </li>
+            <li className={styles.li}>
+              <Link to="/projects" onClick={toggleMenu}>
+                Projects
+              </Link>
+            </li>
+            <li className={styles.li}>
+              <Link to="/resume" onClick={toggleMenu}>
+                Resume
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div
+          className={`${styles.hamburger} ${MenuToggle ? styles.active : ""}`} // Add active class
+          onClick={toggleMenu}
+        >
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+        </div>
+      </header>
+    </>
+  );
 }
 
-export default Header
+export default Header;
